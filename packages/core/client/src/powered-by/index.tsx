bd82be1f48f2dbbfe98600650dc6243b5501a77b
Ulvi <ulva95@gmail.com>
2025-03-15 00:30:04 +0300

    minor

--- packages/core/client/src/powered-by/index.tsx
+++ packages/core/client/src/powered-by/index.tsx
@@ -40,11 +40,7 @@ export const PoweredBy = () => {
     <div
       className={cx(style, 'nb-brand')}
       dangerouslySetInnerHTML={{
-        __html: parseHTML(
-          customBrandPlugin?.options?.options?.brand ||
-            `Powered by <a href="${urls[i18n.language] || urls['en-US']}" target="_blank">NocoBase</a>`,
-          { appVersion },
-        ),
+        __html: parseHTML(customBrandPlugin?.options?.options?.brand || `Разарботано в МИСиС`, { appVersion }),
       }}
     ></div>
   );
